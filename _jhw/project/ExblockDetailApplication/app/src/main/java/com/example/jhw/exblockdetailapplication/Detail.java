9fa5b5b82ed3a73b7ec9af8268b726a6a9661574
sparrow <isc9683@naver.com>
2019-09-09 00:46:01 +0900

    project 9/8 (누락된 파일 추가)
    - 깃허브 폴더 수정
    - 현재위치 표시 추가 [미완성]
    * README에 미완성 리스트 추가 예정

--- _jhw/project/ExblockDetailApplication/app/src/main/java/com/example/jhw/exblockdetailapplication/Detail.java
+++ _jhw/project/ExblockDetailApplication/app/src/main/java/com/example/jhw/exblockdetailapplication/Detail.java
@@ -1,16 +1,9 @@
 package com.example.jhw.exblockdetailapplication;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-import android.view.View;
-import android.widget.TextView;
-
-import com.google.gson.annotations.SerializedName;
-
 public class Detail {
     PoiDetailInfo poiDetailInfo;
 
-    public static class PoiDetailInfo implements Parcelable {
+    public static class PoiDetailInfo {
         private String id;
         private String name;
         private String address;
@@ -22,7 +15,6 @@ public class Detail {
         private String parkFlag;
         private String additionalInfo;
         private String desc;
-
 
         public String getId() {
             return id;
@@ -67,53 +59,6 @@ public class Detail {
         public String getDesc() {
             return desc;
         }
-
-        @Override
-        public int describeContents() {
-            return 0;
-        }
-
-        @Override
-        public void writeToParcel(Parcel parcel, int i) {
-            parcel.writeString(this.id);
-            parcel.writeString(this.name);
-            parcel.writeString(this.address);
-            parcel.writeString(this.firstNo);
-            parcel.writeString(this.secondNo);
-            parcel.writeString(this.tel);
-            parcel.writeDouble(this.lat);
-            parcel.writeDouble(this.lon);
-            parcel.writeString(this.parkFlag);
-            parcel.writeString(this.additionalInfo);
-            parcel.writeString(this.desc);
-        }
-
-        protected PoiDetailInfo(Parcel in) {
-            this.id = in.readString();
-            this.name = in.readString();
-            this.address = in.readString();
-            this.firstNo = in.readString();
-            this.secondNo = in.readString();
-            this.tel = in.readString();
-            this.lat = in.readDouble();
-            this.lon = in.readDouble();
-            this.parkFlag = in.readString();
-            this.additionalInfo = in.readString();
-            this.desc = in.readString();
-        }
-
-        public static final Creator<PoiDetailInfo> CREATOR = new Creator<PoiDetailInfo>() {
-            @Override
-            public PoiDetailInfo createFromParcel(Parcel in) {
-                return new PoiDetailInfo(in);
-            }
-
-            @Override
-            public PoiDetailInfo[] newArray(int size) {
-                return new PoiDetailInfo[size];
-            }
-        };
-
     }
 
     public PoiDetailInfo getPoiDetailInfo() {
--- _jhw/project/ExblockDetailApplication/app/src/main/java/com/example/jhw/exblockdetailapplication/WaitingDialog.java
+++ _jhw/project/ExblockDetailApplication/app/src/main/java/com/example/jhw/exblockdetailapplication/WaitingDialog.java
@@ -7,8 +7,6 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 
-import java.util.logging.Logger;
-
 public class WaitingDialog {
 
     private static final Handler mainHandler = new Handler(Looper.getMainLooper());
