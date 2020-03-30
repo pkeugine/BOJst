a9fffd42e6fce731b6103fa175c53a7e1589300a
Eugine Park <pkeugine@gmail.com>
2020-03-30 23:45:39 +0900

    1181

---
+++ _pyj/1181.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool comp(const string& s1, const string& s2) {
+  if(s1.size() == s2.size()) {
+    return s1 < s2;
+  }
+  return s1.size() < s2.size();
+}
+
+int main() {
+  vector<string> v;
+  int n;
+  
+  cin >> n;
+
+  string str;
+
+  for(int i=0; i<n; i++) {
+    cin >> str;
+    v.push_back(str);
+  }
+
+  vector<string>::iterator iter;
+  vector<string>::iterator end_iter;
+
+  sort(v.begin(), v.end(), comp);
+
+  end_iter = unique(v.begin(), v.end());
+
+  for(iter=v.begin(); iter!=end_iter; iter++) {
+    cout << *iter << endl;
+  }
+  
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
