fa73e4d0978837f7860ebede7bfc20e2b279bbc0
Eugine Park <pkeugine@gmail.com>
2020-04-09 00:01:44 +0900

    add peg solitaire

---
+++ _pyj/z_practice.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  var a = "this is a variable"
+  fmt.Println(a)
+
+  b := 'b'
+  fmt.Println(b)
+
+  var c, d int = 3, 4
+  fmt.Println(c+d)
+
+  var (
+    e = "apple"
+  )
+  fmt.Println(e)
+}
