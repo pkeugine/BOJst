5d77e931ec7d17a7a2e56710378a5275cfe0ccd3
Eugine Park <pkeugine@gmail.com>
2020-04-22 02:39:36 +0900

    fix vim tab error, add c sharp function 001

---
+++ _pyj/csharpfunction001.cs
@@ -0,0 +1,23 @@
+using System;
+class funcexer1
+{
+    public static void welcome()
+    {
+        Console.WriteLine("Welcome Friends!");
+    }
+
+    public static void HaveAnice()
+    {
+        Console.WriteLine("Have a nice day!");
+    }
+
+    public static void Main()
+    {
+        Console.Write("\n\nSee, how to create an user define function :\n");
+        Console.Write("--------------------------------------------------");
+
+        welcome();
+        HaveAnice();
+        Console.Write("\n");
+    }
+}
